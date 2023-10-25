adca301fba8fea6574de20cee09b2364ffe6afee
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-10-25 12:54:45 +0500

    created utils and app.js

--- frontend/blog/src/App.jsx
+++ frontend/blog/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Create from './pages/create/Create';
+import BlogDetails from './pages/blogDetails/BlogDetails';
+import UpdateBlog from './pages/updateBlog/UpdateBlog';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user } = useSelector((state) => state.auth)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
+        <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+        <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
+        <Route path='/create' element={user ? <Create /> : <Navigate to='/login' />} />
+        <Route path='/blogDetails/:id' element={user ? <BlogDetails /> : <Navigate to='/login' />} />
+        <Route path='/updateBlog/:id' element={user ? <UpdateBlog /> : <Navigate to='/login' />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
